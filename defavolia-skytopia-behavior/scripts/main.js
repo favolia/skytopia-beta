60fbe27e436d82eea2d474b2089e635f2f5f0bcf
dais <98775120+favolia@users.noreply.github.com>
2023-08-09 16:47:08 +0800

    fix main.js

--- defavolia-skytopia-behavior/scripts/main.js
+++ defavolia-skytopia-behavior/scripts/main.js
@@ -1,49 +1,49 @@
-import { world as Skytopia, system } from "@minecraft/server";
-import { showActionFormData } from "./functions/Modal/ActionFormData";
-
-Skytopia.afterEvents.playerSpawn.subscribe(ev => {
-    const { initialSpawn, player } = ev
-    if (initialSpawn && !player.hasTag('survival')) {
-        system.runTimeout(() => {
-            showActionFormData(player)
-        }, 60)
-    }
-
-})
-
-system.runInterval(() => {
-    const mcPlayer = Skytopia.getPlayers();
-
-    let radius = 3
-
-    const setLocation = {
-        x: -105,
-        y: 75,
-        z: 80
-    };
-
-    radius <= 0 ? 0 : radius - 1
-
-    mcPlayer.every(p => {
-        let { x, y, z } = p.location;
-        
-        const digitX = String(x).split('.')[0];
-        const digitY = String(y).split('.')[0];
-        const digitZ = String(z).split('.')[0];
-    
-        const radiusEntry = Math.abs(digitX - setLocation.x - 1) <= radius &&
-                            Math.abs(digitY - setLocation.y) <= radius &&
-                            Math.abs(digitZ - setLocation.z) <= radius;
-    
-        if (radiusEntry && !p.hasTag('openMenu')) {
-            p.addTag('openMenu');
-            showActionFormData(p);
-        } else if (!radiusEntry && p.hasTag('openMenu')) {
-            p.removeTag('openMenu');
-        }
-    });
-    
-}, 60);
-
-
-
+import { world as Skytopia, system } from "@minecraft/server"; 
+ import { showActionFormData } from "./functions/Modal/ActionFormData"; 
+  
+ Skytopia.afterEvents.playerSpawn.subscribe(ev => { 
+     const { initialSpawn, player } = ev 
+     if (initialSpawn && !player.hasTag('survival')) { 
+         system.runTimeout(() => { 
+             showActionFormData(player) 
+         }, 60) 
+     } 
+  
+ }) 
+  
+ system.runInterval(() => { 
+     const mcPlayer = Skytopia.getPlayers(); 
+  
+     const setLocation = { 
+         x: -290, 
+         y: 77, 
+         z: 171 
+     }; 
+     
+     let radius = 3;
+     
+  
+     mcPlayer.every(p => { 
+         let { x, y, z } = p.location;
+         radius <= 0 ? 0 : radius -1;
+  
+         const digitX = String(x).split('.')[0]; 
+         const digitY = String(y).split('.')[0]; 
+         const digitZ = String(z).split('.')[0]; 
+  
+         const radiusEntry = Math.abs(digitX - setLocation.x - 1) <= radius && 
+                             Math.abs(digitY - setLocation.y) <= radius && 
+                             Math.abs(digitZ - setLocation.z) <= radius;
+  
+         if (radiusEntry && !p.hasTag('openMenu')) { 
+           Skytopia.sendMessage('kamu di area menu')
+             p.addTag('openMenu'); 
+             showActionFormData(p); 
+         } else if (!radiusEntry && p.hasTag('openMenu')) { 
+           Skytopia.sendMessage('Kamu tidak berada di dalam radius menu')
+             p.removeTag('openMenu'); 
+         } 
+     }); 
+  
+ }, 10); 
+ 
